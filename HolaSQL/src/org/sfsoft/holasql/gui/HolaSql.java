e2a8ffd3a35ad0a6827d6174a3e5ae4e5148a4c7
Santiago Faci <santiago.faci@gmail.com>
2014-11-19 00:37:07 +0100

    Adaptado a IntelliJ

--- HolaSQL/src/org/sfsoft/holasql/gui/HolaSql.java
+++ HolaSQL/src/org/sfsoft/holasql/gui/HolaSql.java
@@ -1,168 +1,125 @@
 package org.sfsoft.holasql.gui;
 
-import java.awt.EventQueue;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-
-import org.sfsoft.holasql.gui.JConecta;
-import org.sfsoft.holasql.gui.JConecta.Accion;
-
 /**
- * Ejemplo que conecta/desconecta con MySQL utilizando el driver JDBC
+ * Aplicaci贸n que comprueba la conexi贸n con MySQL
  * @author Santiago Faci
- *
+ * @version curso 2014-2015
  */
 public class HolaSql {
+    private JLabel lbEstado;
+    private JPanel panelConectar;
 
-	private JFrame frmHolasql;
-	private JMenuBar menuBar;
-	private JMenu mnServidor;
-	private JMenuItem mntmConectar;
-	private JMenuItem mntmDesconectar;
-	private JMenuItem mntmSalir;
-	
-	private Connection conexion;
+    private Connection conexion;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					HolaSql window = new HolaSql();
-					window.frmHolasql.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public HolaSql() {
 
-	/**
-	 * Create the application.
-	 */
-	public HolaSql() {
-		initialize();
-	}
-	
-	/*
-	 * Carga el driver JDBC para MySQL y conecta con el SGBD
-	 */
-	private void conectar() {
-		
-		/*
-		 * Muestra la ventana de conexi髇 y recoge los datos del usuario. La ventana es modal
+    }
+
+    /**
+     * Crea una barra de men煤
+     * @return La barra de men煤 creada
+     */
+    public JMenuBar getMenuBar() {
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Servidor");
+        menuBar.add(menu);
+        JMenuItem menuItem = new JMenuItem("Conectar");
+            menuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    conectar();
+                }
+            });
+        menu.add(menuItem);
+        menuItem = new JMenuItem("Desconectar");
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                desconectar();
+            }
+        });
+        menu.add(menuItem);
+        menuItem = new JMenuItem("Salir");
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                salir();
+            }
+        });
+        menu.add(menuItem);
+
+        return menuBar;
+    }
+
+    private void conectar() {
+
+        /*
+		 * Muestra la ventana de conexi贸n y recoge los datos del usuario. La ventana es modal
 		 * por lo que espera hasta que el usuario la cierre
 		 */
-		JConecta jConecta = new JConecta();
-		if (jConecta.mostrarDialogo() == Accion.CANCELAR)
-			return;
-		
-		String servidor = jConecta.getServidor();
-		String usuario = jConecta.getUsuario();
-		String contrasena = jConecta.getContrasena();
-		
-		try {
+        JConecta dialogoConexion = new JConecta();
+        if (dialogoConexion.mostrarDialogo() == JConecta.Accion.CANCELAR)
+            return;
+
+        String servidor = dialogoConexion.getHost();
+        String usuario = dialogoConexion.getUsuario();
+        String contrasena = dialogoConexion.getContrasena();
+
+        try {
 			/*
-			 * Carga el driver de conexi髇 JDBC para conectar con MySQL
+			 * Carga el driver de conexi贸n JDBC para conectar con MySQL
 			 */
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			conexion = DriverManager.getConnection("jdbc:mysql://" + servidor + ":3306" + "/", usuario, contrasena);
-			JOptionPane.showMessageDialog(null, "Se ha conectado con 閤ito");
-			
-			mntmConectar.setEnabled(false);
-			mntmDesconectar.setEnabled(true);
-			
-		} catch (ClassNotFoundException cnfe) {
-			JOptionPane.showMessageDialog(null, "No se ha podido cargar el driver de la Base de Datos");
-		} catch (SQLException sqle) {
-			JOptionPane.showMessageDialog(null, "No se ha podido conectar con la Base de Datos");
-			sqle.printStackTrace();
-		} catch (InstantiationException ie) {
-			ie.printStackTrace();
-		} catch (IllegalAccessException iae) {
-			iae.printStackTrace();
-		}
-	}
-	
-	/*
-	 * Desconecta de MySQL
-	 */
-	private void desconectar() {
-		
-		try {
-			conexion.close();
-			conexion = null;	
-			
-			JOptionPane.showMessageDialog(null, "Se ha desconectado de la Base de Datos");
-			
-			mntmConectar.setEnabled(true);
-			mntmDesconectar.setEnabled(false);
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-		}
-		
-	}
-	
-	/*
-	 * Sale de la aplicaci髇
-	 */
-	private void salir() {
-		
-		desconectar();
-		System.exit(0);
-	}
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:mysql://" + servidor + ":3306" + "/", usuario, contrasena);
+            JOptionPane.showMessageDialog(null, "Se ha conectado con 茅xito");
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmHolasql = new JFrame();
-		frmHolasql.setTitle("HolaSql");
-		frmHolasql.setBounds(100, 100, 450, 300);
-		frmHolasql.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmHolasql.setLocationRelativeTo(null);
-		
-		menuBar = new JMenuBar();
-		frmHolasql.setJMenuBar(menuBar);
-		
-		mnServidor = new JMenu("Servidor");
-		menuBar.add(mnServidor);
-		
-		mntmConectar = new JMenuItem("Conectar");
-		mntmConectar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				conectar();
-			}
-		});
-		mnServidor.add(mntmConectar);
-		
-		mntmDesconectar = new JMenuItem("Desconectar");
-		mntmDesconectar.setEnabled(false);
-		mntmDesconectar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				desconectar();
-			}
-		});
-		mnServidor.add(mntmDesconectar);
-		
-		mntmSalir = new JMenuItem("Salir");
-		mntmSalir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				salir();
-			}
-		});
-		mnServidor.add(mntmSalir);
-	}
+        } catch (ClassNotFoundException cnfe) {
+            JOptionPane.showMessageDialog(null, "No se ha podido cargar el driver de la Base de Datos");
+        } catch (SQLException sqle) {
+            JOptionPane.showMessageDialog(null, "No se ha podido conectar con la Base de Datos");
+            sqle.printStackTrace();
+        } catch (InstantiationException ie) {
+            ie.printStackTrace();
+        } catch (IllegalAccessException iae) {
+            iae.printStackTrace();
+        }
+    }
 
+    private void desconectar() {
+
+        try {
+            conexion.close();
+            conexion = null;
+
+            JOptionPane.showMessageDialog(null, "Se ha desconectado de la Base de Datos");
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    private void salir() {
+
+        desconectar();
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HolaSql");
+        HolaSql panel = new HolaSql();
+        frame.setJMenuBar(panel.getMenuBar());
+        frame.setContentPane(panel.panelConectar);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
--- HolaSQL/src/org/sfsoft/holasql/gui/JConecta.java
+++ HolaSQL/src/org/sfsoft/holasql/gui/JConecta.java
@@ -1,159 +1,97 @@
 package org.sfsoft.holasql.gui;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * Diálogo para recoger los datos de conexión con un SGBD
+ * DiÃ¡logo para recoger los datos de conexiÃ³n con un SGBD
  * @author Santiago Faci
- * @version 1.0
+ * @version curso 2014-2015
  */
 public class JConecta extends JDialog {
+    private JPanel panel1;
+    private JTextField tfUsuario;
+    private JPasswordField tfContrasena;
+    private JButton btConectar;
+    private JTextField tfHost;
+    private JButton btCancelar;
 
-	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
-	private JTextField txtServidor;
-	private JTextField txtUsuario;
-	private JPasswordField txtContrasena;
-	
-	private String servidor;
-	private String usuario;
-	private String contrasena;
-	public enum Accion {
-		ACEPTAR, CANCELAR
-	}
-	private Accion accion;
-	
-	/**
-	 * El usuario ha pulsado aceptar. Se recogen los datos del formulario como atributos de la clase
-	 * y se esconde el formulario
-	 */
-	private void aceptar() {
-		
-		servidor = txtServidor.getText();
-		usuario = txtUsuario.getText();
-		contrasena = String.valueOf(txtContrasena.getPassword());
-		
-		accion = Accion.ACEPTAR;
-		setVisible(false);
-	}
-	
-	/**
-	 * El usuario cancela. Se esconde el formulario 
-	 */
-	private void cancelar() {
-		
-		accion = Accion.CANCELAR;
-		setVisible(false);
-	}
-	
+    private String host;
+    private String usuario;
+    private String contrasena;
+    public enum Accion {
+        ACEPTAR, CANCELAR
+    }
+    private Accion accion;
+
+    public JConecta() {
+
+        setContentPane(panel1);
+        pack();
+        setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        setModal(true);
+        setLocationRelativeTo(null);
+
+        btConectar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aceptar();
+            }
+        });
+
+        btCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelar();
+            }
+        });
+    }
+
+    /**
+     * El usuario ha pulsado aceptar. Se recogen los datos del formulario como atributos de la clase
+     * y se esconde el formulario
+     */
+    private void aceptar() {
+
+        host = tfHost.getText();
+        usuario = tfUsuario.getText();
+        contrasena = String.valueOf(tfContrasena.getPassword());
+
+        accion = Accion.ACEPTAR;
+        setVisible(false);
+    }
+
+    /**
+     * El usuario cancela. Se esconde el formulario
+     */
+    private void cancelar() {
+
+        accion = Accion.CANCELAR;
+        setVisible(false);
+    }
+
 	/*
-	 * Getters para la recogida de información del formulario desde la ventana principal 
-	 * de la aplicación
+	 * Getters para la recogida de informaciÃ³n del formulario desde la ventana principal
+	 * de la aplicaciÃ³n
 	 */
-	
-	public String getServidor() {
-		return servidor;
-	}
-	
-	public String getUsuario() {
-		return usuario;
-	}
-	
-	public String getContrasena() {
-		return contrasena;
-	}
-	
-	public Accion mostrarDialogo() {
-		
-		setVisible(true);
-		
-		return accion;
-	}
-	
-	/**
-	 * Crea el diálogo y lo muestra en pantalla
-	 */
-	public JConecta() {
-		setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		/*
-		 * Hace la ventana modal. Bloquea la aplicación hasta que el usuario
-		 * cierre la ventana
-		 */
-		setModal(true);
-		setTitle("Conectar");
-		setBounds(100, 100, 229, 191);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		
-		JLabel lblServidor = new JLabel("Servidor");
-		lblServidor.setBounds(10, 26, 66, 14);
-		contentPanel.add(lblServidor);
-		
-		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(10, 51, 66, 14);
-		contentPanel.add(lblUsuario);
-		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(10, 76, 66, 14);
-		contentPanel.add(lblContrasea);
-		
-		txtServidor = new JTextField();
-		txtServidor.setBounds(89, 23, 106, 20);
-		contentPanel.add(txtServidor);
-		txtServidor.setColumns(10);
-		
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(89, 48, 106, 20);
-		contentPanel.add(txtUsuario);
-		txtUsuario.setColumns(10);
-		
-		txtContrasena = new JPasswordField();
-		txtContrasena.setBounds(89, 73, 106, 20);
-		contentPanel.add(txtContrasena);
-		txtContrasena.setColumns(10);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						aceptar();
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						cancelar();
-					}
-				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
-		
-		/*
-		 * Coloca la ventana en el centro de la pantalla
-		 */
-		setLocationRelativeTo(null);
-	}
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public Accion mostrarDialogo() {
+
+        setVisible(true);
+
+        return accion;
+    }
 }
